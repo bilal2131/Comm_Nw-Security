a9e9fe982eb3b95cf29157ac3a4c3e357d4258bc
bilal2131 <95859274+bilal2131@users.noreply.github.com>
2022-03-29 20:49:17 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,19 @@
-print("hello")
+import re
+from flask import Flask
+from flask import request, render_template, session
+from flask import redirect, url_for
+from flask_session import Session
+from flask_mail import Mail,Message
+import hashlib
+import os
+from string_utility import *
+import cryptocode
+
+#Store the data on memory(volatile)
+data_dict={}
+#init the flask app instance
+app=Flask(name)
+#session configuration
+app.config['SESSION PERMANENT'] = True
+app.config['SESSION_TYPE'] = 'filesystem'
+
