2a05a195fe6bf5029b5bea9a43a64f4276a48cea
bilal2131 <95859274+bilal2131@users.noreply.github.com>
2022-03-30 22:01:04 +0100

    Update server.py

--- server.py
+++ server.py
@@ -16,4 +16,13 @@ app=Flask(name)
 #session configuration
 app.config['SESSION PERMANENT'] = True
 app.config['SESSION_TYPE'] = 'filesystem'
+Session(app) #add cookies/session functionality for temp Storage
+
+#SMTP Configuration
+app.config['MAIL_SERVER']='smtp.gmail.com
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'bilal.it@gmail.com'
+app.config['MAIL_PASSWORD'] = 'zhqvqecchqghwpnp'
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
