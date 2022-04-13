ab244e5676706f338baeceb1d862ce6d95ba55bc
bilal2131 <95859274+bilal2131@users.noreply.github.com>
2022-04-13 14:34:48 +0100

    Update crypt_test.py

--- crypt_test.py
+++ crypt_test.py
@@ -1,1 +1,12 @@
 
+# this modile is to test the encryption
+import cyptocode
+
+key = 'test key'
+message = 'test message that can be long and broad'
+
+enc = cryptocode.encrypt(message=message, password=key)
+print(enc)
+dec = cryptocode.decrypt(enc,password=key)
+print(dec)
+
